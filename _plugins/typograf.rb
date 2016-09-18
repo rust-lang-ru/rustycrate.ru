948d16c67d1f660cb864b83f6c565f343427b54b
Michael Pankov <work@michaelpankov.com>
2016-09-18 23:12:29 +0300

    Включаем типограф снова
    Баг с обрезанием файлов починили

--- _plugins/typograf.rb
+++ _plugins/typograf.rb
@@ -1,16 +1,16 @@
-# def typografy(file)
-#   if file.end_with?('.html')
-#     stdout = `typograf -l ru #{file}`
-#     File.write(file, stdout)
-#   end
-# end
+def typografy(file)
+  if file.end_with?('.html')
+    stdout = `typograf -l ru #{file}`
+    File.write(file, stdout)
+  end
+end
 
-# Jekyll::Hooks.register :pages, :post_write do |document|
-#   file = document.destination(document.site.dest)
-#   typografy(file)
-# end
+Jekyll::Hooks.register :pages, :post_write do |document|
+  file = document.destination(document.site.dest)
+  typografy(file)
+end
 
-# Jekyll::Hooks.register :posts, :post_write do |document|
-#   file = document.destination(document.site.dest)
-#   typografy(file)
-# end
+Jekyll::Hooks.register :posts, :post_write do |document|
+  file = document.destination(document.site.dest)
+  typografy(file)
+end
