2d30232eca4484c0e416687c83b58c5f83704c68
Michael Pankov <work@michaelpankov.com>
2016-09-19 22:39:57 +0300

    Выключаем ёфикацию, т.к. там тот же баг

--- _plugins/eyo.rb
+++ _plugins/eyo.rb
@@ -1,16 +1,16 @@
-def eyoify(file)
-  if file.end_with?('.html')
-    stdout = `eyo #{file}`
-    File.write(file, stdout)
-  end
-end
+# def eyoify(file)
+#   if file.end_with?('.html')
+#     stdout = `eyo #{file}`
+#     File.write(file, stdout)
+#   end
+# end
 
-Jekyll::Hooks.register :pages, :post_write do |document|
-  file = document.destination(document.site.dest)
-  eyoify(file)
-end
+# Jekyll::Hooks.register :pages, :post_write do |document|
+#   file = document.destination(document.site.dest)
+#   eyoify(file)
+# end
 
-Jekyll::Hooks.register :posts, :post_write do |document|
-  file = document.destination(document.site.dest)
-  eyoify(file)
-end
+# Jekyll::Hooks.register :posts, :post_write do |document|
+#   file = document.destination(document.site.dest)
+#   eyoify(file)
+# end
