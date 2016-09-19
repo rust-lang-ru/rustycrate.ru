a097d53891e9649af7e3b9e82883b15f59ab8d0b
Michael Pankov <work@michaelpankov.com>
2016-09-19 22:54:53 +0300

    Делаем разные режимы сборки

--- _plugins/eyo.rb
+++ _plugins/eyo.rb
@@ -1,16 +1,20 @@
-# def eyoify(file)
-#   if file.end_with?('.html')
-#     stdout = `eyo #{file}`
-#     File.write(file, stdout)
-#   end
-# end
+def eyoify(file)
+  if file.end_with?('.html')
+    stdout = `eyo #{file}`
+    File.write(file, stdout)
+  end
+end
 
-# Jekyll::Hooks.register :pages, :post_write do |document|
-#   file = document.destination(document.site.dest)
-#   eyoify(file)
-# end
+if ENV['RUSTYCRATE_MODE'] == 'production' and
+   ENV['RUSTYCRATE_ENABLE_EYO'] == 'yes'
 
-# Jekyll::Hooks.register :posts, :post_write do |document|
-#   file = document.destination(document.site.dest)
-#   eyoify(file)
-# end
+  Jekyll::Hooks.register :pages, :post_write do |document|
+    file = document.destination(document.site.dest)
+    eyoify(file)
+  end
+
+  Jekyll::Hooks.register :posts, :post_write do |document|
+    file = document.destination(document.site.dest)
+    eyoify(file)
+  end
+end
