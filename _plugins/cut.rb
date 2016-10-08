648cf797404fa936feb85fc4ac0d53cbb81085fc
Michael Pankov <work@michaelpankov.com>
2016-10-08 19:43:22 +0300

    Реализуем прокрутку для "читать дальше"

---
+++ _plugins/cut.rb
@@ -0,0 +1,11 @@
+def cut(content)
+  excerpt, cut, body = content.to_s.partition('<cut/>')
+  excerpt +
+    cut +
+    '<div id="after_cut"></div>' +
+    body
+end
+
+Jekyll::Hooks.register :posts, :pre_render do |document|
+  document.content = cut(document.content)
+end
